6248f2ad2a5b7dbbeb20b8c40e6ff70040986bb4
Nadezhda <marypaladio@gmail.com>
2024-10-02 00:47:07 +0300

    adding color notification

--- index.js
+++ index.js
@@ -10,12 +10,24 @@ const hexValues = [
     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F",
 ];
 
-function getREandomHexValue() {
+function getRandomHexValue() {
     const randomIndexPosition = Math.floor(Math.random() * hexValues.length);
-const randomHexValue = hexValues[randomIndexPosition]
+    const randomHexValue = hexValues[randomIndexPosition]
+    return randomHexValue;
+}
+
+
+function getRandomHexString(stringLength) {
+    let hexString = "";
+    for (let i = 0; i < stringLength; i++) {
+        hexString += getRandomHexValue();
+    }
+
+    return hexString;
 }
 
 
 newColourBtnElement.addEventListener("click", function () {
-    alert("ok");
+    const randomHexString = getRandomHexString(6);
+    alert(randomHexString);
 });
